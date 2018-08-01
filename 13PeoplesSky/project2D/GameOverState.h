64a93050d0bb1eefa0b39e85b2f7622ed0251816
Cameron Brown <cameron@cmrn.io>
2018-08-02 08:55:06 +1000

    Re-implement Game Over State functions in header

--- 13PeoplesSky/project2D/GameOverState.h
+++ 13PeoplesSky/project2D/GameOverState.h
@@ -1,10 +1,17 @@
 #pragma once
+
 #include "BaseState.h"
+
 class GameOverState :
 	public BaseState
 {
 public:
 	GameOverState();
 	~GameOverState();
+
+	void Enter() override;
+	void Update(float fDeltaTime, StateMachine* pStateMachine) override;
+	void Draw(aie::Renderer2D* pRenderer) override;
+	void Exit() override;
 };
 
