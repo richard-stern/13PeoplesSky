e5bc53c36cd72e061cbdd6a54cd7b49025417972
jackdawes09 <36465192+jackdawes09@users.noreply.github.com>
2018-08-02 09:33:14 +1000

    Completed the GameState.cpp

--- 13PeoplesSky/project2D/GameState.cpp
+++ 13PeoplesSky/project2D/GameState.cpp
@@ -4,31 +4,46 @@
 
 GameState::GameState()
 {
-
+	
 }
 
 GameState::~GameState()
 {
+	GUI::Destroy();
 
+	if (level)
+	{
+		delete level;
+		level = nullptr;
+	}
 }
 
 void GameState::Enter()
 {
-	
+	level = new Level;
+	GUI::GetInstance();
 }
 
 void GameState::Update(float fDeltaTime, StateMachine* pStateMachine)
 {
+	level->Update(fDeltaTime);
 
 }
 
 void GameState::Draw(aie::Renderer2D* pRenderer)
 {
-
+	level->Draw(pRenderer);
+	GUI::GetInstance()->Draw(pRenderer);
 }
 
 void GameState::Exit()
 {
+	GUI::Destroy();
 
+	if (level)
+	{
+		delete level;
+		level = nullptr;
+    }
 }
 
--- 13PeoplesSky/project2D/GameState.h
+++ 13PeoplesSky/project2D/GameState.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "BaseState.h"
+
+
+class Level;
 class GameState : public BaseState
 {
 public:
@@ -9,5 +12,7 @@ public:
 	void Update(float fDeltaTime, StateMachine* pStateMachine);
 	void Draw(aie::Renderer2D* pRenderer);
 	void Exit();
+private:
+	Level* level;
 };
 
