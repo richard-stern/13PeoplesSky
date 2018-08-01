261b57161201eb00229573ff6184acc5184de62e
JakobiTalbot <36465136+JakobiTalbot@users.noreply.github.com>
2018-08-01 16:46:33 +1000

    Updated MenuState

--- 13PeoplesSky/project2D/MenuState.cpp
+++ 13PeoplesSky/project2D/MenuState.cpp
@@ -1,5 +1,8 @@
 #include "MenuState.h"
-
+#include "StateMachine.h"
+#include "TextureManager.h"
+#include "Input.h"
+#include "Vector2.h"
 MenuState::MenuState()
 {
 }
@@ -10,20 +13,49 @@ MenuState::~MenuState()
 
 void MenuState::Enter()
 {
+	// EVERYTHING HERE IS PLACEHOLDER
 
+	// Allocate button matrices
+	m_m3StartButton = new Matrix3();
+	m_m3QuitButton = new Matrix3();
+
+	// Set button positions
+	m_m3StartButton->SetPosition(640, 500);
+	m_m3QuitButton->SetPosition(640, 300);
+
+	// Load button textures
+	TextureManager* textureMan = TextureManager::GetInstance();
+	m_pStartTexture = textureMan->LoadTexture("./textures/rock_large.png");
+	m_pQuitTexture = textureMan->LoadTexture("./textures/rock_medium.png");
 }
 
-void MenuState::Update(float fDeltaTime)
+void MenuState::Update(float fDeltaTime, StateMachine* pStateMachine)
 {
+	aie::Input* pInput = aie::Input::getInstance();
+	if (pInput->wasMouseButtonPressed(aie::INPUT_MOUSE_BUTTON_LEFT))
+	{
+		Vector2 v2MousePos = Vector2(pInput->getMouseX(), pInput->getMouseY());
 
+		// USE MOUSE POS TO FIND IF OVER BUTTON
+
+	}
 }
 
 void MenuState::Draw(aie::Renderer2D* pRenderer)
 {
-
+	// Draw start button
+	pRenderer->drawSpriteTransformed3x3(m_pStartTexture, (float*)m_m3StartButton);
+	
+	// Draw quit button
+	pRenderer->drawSpriteTransformed3x3(m_pQuitTexture, (float*)m_m3QuitButton);
 }
 
 void MenuState::Exit()
 {
+	// Delete assets
+	delete m_m3StartButton;
+	m_m3StartButton = nullptr;
 
+	delete m_m3QuitButton;
+	m_m3QuitButton = nullptr;
 }
--- 13PeoplesSky/project2D/MenuState.h
+++ 13PeoplesSky/project2D/MenuState.h
@@ -1,13 +1,48 @@
 #pragma once
 #include "BaseState.h"
+#include "Matrix3.h"
+#include "Texture.h"
 class MenuState : public BaseState
 {
 public:
 	MenuState();
 	~MenuState();
 
+	//----------------------------------------------
+	// Enter
+	//----------------------------------------------
+	// Initialise state assets
+	// Called when state starts
+	// Basically a constructor for the class
 	void Enter();
-	void Update(float fDeltaTime);
+
+	//----------------------------------------------
+	// Update
+	//----------------------------------------------
+	// Processes input
+	// Updates assets
+	// Changes StateMachine state when criteria is met
+	// Called each frame
+	void Update(float fDeltaTime, StateMachine* pStateMachine);
+
+	//----------------------------------------------
+	// Draw
+	//----------------------------------------------
+	// Draws each asset in state
+	// Called each frame
 	void Draw(aie::Renderer2D* pRenderer);
+
+	//----------------------------------------------
+	// Exit
+	//----------------------------------------------
+	// Deletes state assets
+	// Called when state ends
+	// Basically a destructor for the class
 	void Exit();
+
+private:
+	Matrix3* m_m3StartButton;
+	Matrix3* m_m3QuitButton;
+	aie::Texture* m_pStartTexture;
+	aie::Texture* m_pQuitTexture;
 };
