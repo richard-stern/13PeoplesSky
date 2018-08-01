167f5f9959eeb80257c637f3ef2cd322635dfc30
DillonHandyside <dj_handyside@hotmail.com>
2018-08-01 17:19:15 +1000

    Actor 2nd commit. GameObject references uncommented. Collision still commented

--- 13PeoplesSky/project2D/Actor.cpp
+++ 13PeoplesSky/project2D/Actor.cpp
@@ -1,30 +1,30 @@
-//#include "Actor.h"
-//
-//Actor::Actor()
-//{
-//	m_nMaxHealth = 100;
-//	m_nHealth = m_nMaxHealth;
-//}
-//
-//Actor::Actor(Matrix3 *m_m3StartPosition)
-//{
-//	//m_m3GlobalMatrix = m_m3StartPosition;
-//	m_nMaxHealth = 100;
-//	m_nHealth = m_nMaxHealth;
-//}
-//
-//Actor::~Actor()
-//{
-//}
-//
-///*
-//Function:	 Update
-//Input/s:	 float
-//Description: Calls base class (GameObject) Update function. Then updates attached
-//			 collider object (bounding boxes & other collision data)
-//*/
-//void Actor::Update(float deltaTime)
-//{
-//	//GameObject::Update(deltaTime);
-//	m_pCollider.Update(deltaTime);
-//}
+#include "Actor.h"
+
+Actor::Actor()
+{
+	m_nMaxHealth = 100;
+	m_nHealth = m_nMaxHealth;
+}
+
+Actor::Actor(Matrix3 m_m3StartPosition)
+{
+	m_m3GlobalMatrix = m_m3StartPosition;
+	m_nMaxHealth = 100;
+	m_nHealth = m_nMaxHealth;
+}
+
+Actor::~Actor()
+{
+}
+
+/*
+Function:	 Update
+Input/s:	 float
+Description: Calls base class (GameObject) Update function. Then updates attached
+			 collider object (bounding boxes & other collision data)
+*/
+void Actor::Update(float deltaTime)
+{
+	GameObject::Update(deltaTime);
+	//m_pCollider.Update(deltaTime);
+}
