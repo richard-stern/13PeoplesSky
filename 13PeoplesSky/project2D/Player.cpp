03125f8c4d2ad90ecb32e9f54b7febcfb209c5a9
gabe108 <36465177+gabe108@users.noreply.github.com>
2018-08-01 17:15:48 +1000

    Player Added with linear motion move the player by WASD

--- 13PeoplesSky/project2D/Player.cpp
+++ 13PeoplesSky/project2D/Player.cpp
@@ -1,12 +1,65 @@
+#include <Input.h>
 #include "Player.h"
-
-
+//#include "TextureManager.h"
 
 Player::Player()
 {
+	// Requests a texture from TextureManager
+	m_m3LocalMatrix.SetPosition(Vector2(600.0f, 600.0f));
+	m_v2Velocity = Vector2(0.0f, 0.0f);
+	m_fMaxSpeed = 100.0f;
+	m_timer = 0.0f;
+	m_fMass = 0.5f;
+	m_fDrag = 0.5f;
 }
-
 
 Player::~Player()
 {
+	
 }
+
+void Player::update(float deltaTime)
+{
+	aie::Input* input = aie::Input::getInstance();
+
+	Matrix3 mScale;
+	Vector2 v2Pos = m_m3LocalMatrix.GetPosition();
+	Vector2 v2Acceleration = Vector2(0.0f, 0.0f);
+	Vector2 v2Dampening = Vector2(0.0f, 0.0f);
+	Vector2 v2ForceSum = Vector2(0.0f, 0.0f);
+
+
+	//Actor::update(deltaTime, 150.0f);
+	// use arrow keys to move camera
+	if (input->isKeyDown(aie::INPUT_KEY_W))
+		v2ForceSum.y += m_fMaxSpeed;
+		
+	if (input->isKeyDown(aie::INPUT_KEY_S))
+		v2ForceSum.y -= m_fMaxSpeed;
+
+	if (input->isKeyDown(aie::INPUT_KEY_A))
+		v2ForceSum.x -= m_fMaxSpeed; 
+
+	if (input->isKeyDown(aie::INPUT_KEY_D))
+		v2ForceSum.x += m_fMaxSpeed;
+
+	v2Acceleration.x = v2ForceSum.x / m_fMass;
+	v2Acceleration.y = v2ForceSum.y / m_fMass;
+	v2Dampening = (m_v2Velocity * m_fDrag) * -1.0f;
+	m_v2Velocity += (v2Acceleration + v2Dampening) * deltaTime;
+	v2Pos = v2Pos + (m_v2Velocity * deltaTime);
+	mPosition.SetPosition(v2Pos);
+	m_m3LocalMatrix = mScale * mPosition;
+
+	// update GlobalTransform
+	//m_cLevelCamera.SetPosition();
+}
+
+void Player::draw(aie::Renderer2D* pRenderer)
+{
+	//insert GlobalTransform
+	pRenderer->drawSpriteTransformed3x3(nullptr, (float*)m_m3LocalMatrix, 50.0f, 50.0f);
+}
+
+int Player::GetHealth(){ return 100; } //TODO Return Health
+//void Player::SetCamera(Camera * LevelCamera){ m_cLevelCamera = LevelCamera; }
--- 13PeoplesSky/project2D/Player.h
+++ 13PeoplesSky/project2D/Player.h
@@ -1,10 +1,33 @@
-#pragma once
-#include "Actor.h"
-class Player :
-	public Actor
+//#include Actor.h
+#include "Matrix3.h"
+#include "Vector2.h"
+#include <Renderer2D.h>
+#include <Texture.h>
+
+//class Camera;
+
+class Player //: public Actor
 {
 public:
 	Player();
 	~Player();
-};
 
+	void update(float deltTime);
+	void draw(aie::Renderer2D* pRenderer);
+
+	int GetHealth();
+	//void SetCamera(Camera* LevelCamera);
+
+protected:
+	//Camera* m_cLevelCamera;
+
+	// Replace under variables with gameobject's variables
+	Matrix3 m_m3LocalMatrix;
+	Matrix3 m_m3GlobalMatrix;
+	Matrix3 mPosition;
+	Vector2 m_v2Velocity;
+	float m_fMaxSpeed;
+	float m_timer;
+	float m_fDrag;
+	float m_fMass;
+};
