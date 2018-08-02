87af5a4e4783ad43c06ac81b8dd5501407acd29d
Cameron Brown <cameron@cmrn.io>
2018-08-02 11:25:10 +1000

    Game Over state shows the score

--- 13PeoplesSky/project2D/GameOverState.cpp
+++ 13PeoplesSky/project2D/GameOverState.cpp
@@ -1,13 +1,48 @@
 #include "GameOverState.h"
 
-GameOverState::GameOverState() { }
+#include <cassert>
+
+#include "GUI.h"
+#include "Camera.h"
+#include "TextureManager.h"
+
+GameOverState::GameOverState() 
+{
+	TextureManager* manager = TextureManager::GetInstance();
+	assert(manager && "Texture manager should exist!");
+
+	m_font = manager->LoadFont("./font/consolas.ttf", 16);
+}
 
 GameOverState::~GameOverState() { }
 
-void GameOverState::Enter() { }
+void GameOverState::Enter() 
+{ 
+	// grab the score from the GUI
+	m_lastScore = GUI::GetInstance()->GetScore();
+}
 
-void GameOverState::Update(float fDeltaTime, StateMachine* pStateMachine) { }
+void GameOverState::Update(float fDeltaTime, StateMachine* pStateMachine) 
+{
+}
 
-void GameOverState::Draw(aie::Renderer2D* pRenderer) { }
+void GameOverState::Draw(aie::Renderer2D* pRenderer) 
+{ 
+	// grab the size of the window 
+	Vector2 windowSize = Camera::GetInstance()->GetResolution();
+
+	// make score text
+	char scoreText[32];
+	sprintf(scoreText, "Score: %i", m_lastScore);
+
+	// grab the width of the string so we can center it
+	float scoreWidth = m_font->getStringWidth(scoreText);
+	// and store the center position here for cleanliness
+	float centerPos = windowSize.x / 2.0f - scoreWidth / 2.0f;
+
+	// print it somewhere in the middle of the screen
+	pRenderer->setRenderColour(0xffffffff);
+	pRenderer->drawText(m_font, scoreText, centerPos, 200.0f);
+}
 
 void GameOverState::Exit() { }
--- 13PeoplesSky/project2D/GameOverState.h
+++ 13PeoplesSky/project2D/GameOverState.h
@@ -2,6 +2,15 @@
 
 #include "BaseState.h"
 
+#include <vector>
+
+// little struct to contain both a name and a score for use in the leaderboards
+struct GameOverScore
+{
+	int score;
+	std::string name;
+};
+
 class GameOverState :
 	public BaseState
 {
@@ -13,5 +22,13 @@ public:
 	void Update(float fDeltaTime, StateMachine* pStateMachine) override;
 	void Draw(aie::Renderer2D* pRenderer) override;
 	void Exit() override;
+	
+private:
+	aie::Font* m_font;
+
+	// the latest score
+	int m_lastScore;
+	// list of all scores for high score table
+	std::vector<GameOverScore> m_allScores;
 };
 
