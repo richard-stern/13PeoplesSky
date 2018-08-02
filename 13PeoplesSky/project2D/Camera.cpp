9c01c2f7b67e9dcbd173816bc3a66b3ace63a1d0
NicVanZuylen <nicvanzuylen@gmail.com>
2018-08-02 17:01:13 +1000

    -Added shake effect to the camera which needs to be updated. -Added a AddShakeDuration() function to the Camera class. -Camera shake effect stops playing when the duration is at or below zero.

--- 13PeoplesSky/project2D/Camera.cpp
+++ 13PeoplesSky/project2D/Camera.cpp
@@ -1,11 +1,11 @@
 #include "Camera.h"
+#include <iostream>
 
 Camera* Camera::m_instance = nullptr;
 
 Camera::Camera()
 {
-
-
+	m_fShakeDuration = 0.0f;
 }
 
 Camera::~Camera()
@@ -27,11 +27,42 @@ void Camera::SetPosition(float x, float y)
 {
 	m_CameraPosition.x = x - (m_Resolution.x * 0.5f);
 	m_CameraPosition.y = y - (m_Resolution.y * 0.5f);
+
+	UpdateShake(0.017f);
 }
 
 void Camera::SetPosition(Vector2 position)
 {
 	m_CameraPosition = position - (m_Resolution * 0.5f);
+}
+
+void Camera::UpdateShake(float deltatime) 
+{
+	if (m_fShakeDuration > 0.0f)
+	{
+		// Create random unit vector.
+		Vector2 v2CamShakeOffset
+		(
+			-1.0f + (static_cast<float>(rand() % 101) / 50.0f),
+			-1.0f + (static_cast<float>(rand() % 101) / 50.0f)
+		);
+
+		// Add unit vector to camera position multiplied by a magnitude.
+		m_CameraPosition += v2CamShakeOffset * SHAKE_MAGNITUDE;
+
+		// Count down shake duration.
+		m_fShakeDuration -= deltatime;
+	}
+}
+
+void Camera::AddShakeDuration(float duration) 
+{
+	m_fShakeDuration += duration;
+}
+
+void Camera::SetShakeDuration(float duration) 
+{
+	m_fShakeDuration = duration;
 }
 
 Vector2 Camera::GetPosition()
--- 13PeoplesSky/project2D/Camera.h
+++ 13PeoplesSky/project2D/Camera.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "Vector2.h"
 
+#define SHAKE_MAGNITUDE 5.0f
+
 class Camera
 {
 protected:
@@ -9,11 +11,15 @@ protected:
 	static Camera* m_instance;
 	Vector2 m_CameraPosition;
 	Vector2 m_Resolution;
+	float m_fShakeDuration;
 public:
 	static Camera* GetInstance();
 	static void Destroy();
 	void SetPosition(float x, float y);
 	void SetPosition(Vector2 position);
+	void UpdateShake(float deltatime);
+	void AddShakeDuration(float duration);
+	void SetShakeDuration(float duration);
 	void SetResolution(Vector2 resolution);
 	Vector2 GetResolution();
 	Vector2 GetPosition();
