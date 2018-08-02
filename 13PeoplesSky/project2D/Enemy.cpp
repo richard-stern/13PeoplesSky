ed45e384cb9ed011e5991cba79118e5d86947178
Jake-Ailey <36465180+Jake-Ailey@users.noreply.github.com>
2018-08-02 12:27:16 +1000

    Commit ready for testing, finalised the Enemy.cpp

--- 13PeoplesSky/project2D/Enemy.cpp
+++ 13PeoplesSky/project2D/Enemy.cpp
@@ -1,6 +1,7 @@
 #include "Enemy.h"
 #include "Player.h"
 #include "Rock.h"
+#include "Bullet.h
 #include "TextureManager.h"
 #include "PursueBehaviour.h"
 #include "AvoidBehaviour.h"
@@ -9,7 +10,7 @@
 Enemy::Enemy()
 {
 	pTexture = pTexture->GetInstance();
-	this->SetHealth(100);
+	this->SetHealth(3);
 
 	m_pursue = new PursueBehaviour;
 	m_avoid = new AvoidBehaviour;
@@ -26,18 +27,34 @@ void Enemy::Update(Player* pPlayer, Rock* pRock)
 	//Updates the distance between this class and the player every frame
 	m_distBetween = pPlayer->GetPosition().dot(this->GetPosition());
 
-	//If player is within a certain radius, pursue player
-	if (m_distBetween < 200.0f)
+	//When a destroyed enemy is far enough away from the player, redraw it
+	if (m_distBetween > 800.0f && this->GetVisible() == false)
+	{
+		this->SetVisible(true);
+	}
+
+	//If player is within a certain radius, pursue player. Only pursues if the player is drawn
+	if (m_distBetween < 600.0f && this->GetVisible() == true)
 	{
 		m_pursue->update(pPlayer, this);
+	}
+
+	//If the enemy has been destroyed, it will flee the player so that it can reach a distance where it can "respawn"
+	else if (m_distBetween > 5.0f && this->GetVisible() == false)
+	{
+		m_avoid->update(pPlayer, this);
 	}
 
 	m_avoid->update(pRock, this);	
 }
 
+//When the enemy collides with another object, rather than being "destroyed", it simply becomes invisible and runs away
 void Enemy::OnCollision(Actor* collidingObject, CollisionData* data)
 {
 	this->SetVisible(false);
+
+	//When the enemy collides with an object (being rocks, or the player) "destroy" the enemy and deal 1 damage to the object.
+	collidingObject->ModifyHealth(-1);
 }
 
 float Enemy::GetMaxSpeed()
