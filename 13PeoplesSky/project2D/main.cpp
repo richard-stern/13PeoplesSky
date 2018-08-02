e20ebbeb1c0b7c935de14f79fed20aedcfcf5d5f
Cris-Bwls <36465203+Cris-Bwls@users.noreply.github.com>
2018-08-02 14:36:23 +1000

    Added Memory Leak Checker to "main.cpp"

--- 13PeoplesSky/project2D/main.cpp
+++ 13PeoplesSky/project2D/main.cpp
@@ -1,9 +1,13 @@
+#include <crtdbg.h>
+
 #include "Application2D.h"
 #include "Vector2.h"
 
 
 int main() {
-	
+	// Memory leak checker
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
 	// allocation
 	auto app = new Application2D();
 
