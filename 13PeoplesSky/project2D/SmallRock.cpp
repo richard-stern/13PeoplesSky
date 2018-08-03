c71ca037711899186178300b155776dbe5f48539
ThePyromage <the.pyromagi@gmail.com>
2018-08-03 13:09:34 +1000

    Actually added small rock files

---
+++ 13PeoplesSky/project2D/SmallRock.cpp
@@ -0,0 +1,109 @@
+#include "SmallRock.h"
+#include "TextureManager.h"
+#include "CollisionManager.h"
+#include "Primitives.h"
+#include "GUI.h"
+
+SmallRock::SmallRock()
+{
+	//set health and max health to a random value between 1 and 2
+	SetMaxHealth((rand() % 2) + 1);
+	SetHealth(GetMaxHealth());
+
+	//set self to invisible
+	SetVisible(false);
+
+	//set wrap to true
+	m_bWrapAndRespawn = true;
+
+	//get instance of texture manager
+	TextureManager* textureMan = TextureManager::GetInstance();
+	CollisionManager* collisionMan = CollisionManager::GetInstance();
+
+	//set texture to rock texture
+	//TEMP TEXTURE, replace when new texture made
+	SetTexture(textureMan->LoadTexture("./textures/rock_medium.png"));
+
+	//Sets velocity to zero
+	SetVelocity(Vector2(0, 0));
+
+	//Sets position to zero
+	SetPosition(Vector2(0, 0));
+
+	//Sets random rotation in radians
+	SetRotation(((float)(rand() % 32) - 16) / 10);
+
+	//Creates a collider, sets neccesary variables. 
+	PrimRectangle* collider = new PrimRectangle(32, 32);
+	collider->SetLayer(ECOLLISIONLAYER_ROCK);
+	SetCollider(collider);
+	collisionMan->AddObject(this);
+}
+
+
+SmallRock::~SmallRock()
+{
+}
+
+void SmallRock::OnCollision(Actor * collidingObject, CollisionData * data)
+{
+	Vector2 currentPos = GetPosition();
+	switch (collidingObject->GetCollider()->GetLayer())
+	{
+	case(ECOLLISIONLAYER_NONE):
+		//failsafe code, shouldn't ever be called
+		break;
+	case(ECOLLISIONLAYER_PLAYER):
+		//formula for bouncing off of player
+		currentPos -= data->m_v2Normal * data->m_fPenetration;
+		SetVelocity((GetVelocity() - (2 * (GetVelocity().dot(data->m_v2Normal)) * data->m_v2Normal)));
+		break;
+	case(ECOLLISIONLAYER_BULLET):
+		//take damage from the bullet, bullet should also be destroyed on impact
+		ModifyHealth(-1);
+		if (GetHealth() <= 0)
+		{
+			SetVisible(false);
+			GUI::GetInstance()->AddScore(50);
+		}
+		break;
+	case(ECOLLISIONLAYER_ROCK):
+		//formula for bouncing off of other rocks
+		currentPos -= data->m_v2Normal * data->m_fPenetration;
+		SetVelocity((GetVelocity() - (2 * (GetVelocity().dot(data->m_v2Normal)) * data->m_v2Normal)));
+		break;
+	case(ECOLLISIONLAYER_ENEMY):
+		//formula for bouncing off of enemies
+		currentPos -= data->m_v2Normal * data->m_fPenetration;
+		SetVelocity((GetVelocity() - (2 * (GetVelocity().dot(data->m_v2Normal)) * data->m_v2Normal)));
+		break;
+	case(ECOLLISIONLAYER_HEALTH):
+		//formula for bouncing off of health pickups
+		currentPos -= data->m_v2Normal * data->m_fPenetration;
+		SetVelocity((GetVelocity() - (2 * (GetVelocity().dot(data->m_v2Normal)) * data->m_v2Normal)));
+		break;
+	}
+	SetPosition(currentPos);
+}
+
+void SmallRock::SpawnSelf()
+{
+	SetVelocity(Vector2((float)((rand() % 50) - 25), (float)((rand() % 30) - 50)));
+	SetVisible(true);
+}
+
+bool SmallRock::WrapAndRespawn()
+{
+	GameObject::WrapAndRespawn();
+	return true;
+}
+
+void SmallRock::Update(float deltatime)
+{
+	Actor::Update(deltatime);
+}
+
+void SmallRock::Draw(aie::Renderer2D *renderer)
+{
+	Actor::Draw(renderer);
+}
---
+++ 13PeoplesSky/project2D/SmallRock.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "Actor.h"
+class SmallRock :
+	public Actor
+{
+public:
+	//------------------------------------
+	//Constructor
+	//Should do the following:
+	//Request a texture from texture manager. 
+	//Set a random velocity. 
+	//Set max and current health to three (3). 
+	//------------------------------------
+	SmallRock();
+
+	//------------------------------------
+	//Destructor
+	//Should do the following:
+	//Delete any pointers, don't delete texture. 
+	//------------------------------------
+	~SmallRock();
+
+	//------------------------------------
+	//OnCollision
+	//Should do the following:
+	//Detect whether the collision was caused by a bullet, the player, a health pickup, or another rock. 
+	//If collision was caused by anything other than a bullet, the rock should bounce off without taking damage. 
+	//If a bullet collided, the rock should take one (1) damage. 
+	//If the rock's health is reduced to zero (0), the m_bVisible variable should be set to 'false'.
+	//------------------------------------
+	void OnCollision(Actor* collidingObject, CollisionData* data);
+
+	void SpawnSelf();
+
+	bool WrapAndRespawn();
+
+	void Update(float deltatime);
+	void Draw(aie::Renderer2D *renderer);
+};
+
